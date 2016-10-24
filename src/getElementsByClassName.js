00b8d9a476d4342dfa0d46d30fab6543bdceff60
Paige <paige.vogenthaler@gmail.com>
2016-10-23 19:18:02 -0700

    Add existing file

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className, node){
+  var nodes = [];
+  node = node || document.body;
+
+  //Base Case: if node has class, add node to array
+  if(node.classList.contains(className)){
+    nodes.push(node);
+  }
+
+  //Recursive: check node's children for class
+  for(var i = 0; i < node.children.length; i++){
+    nodes = nodes.concat(getElementsByClassName(className, node.children[i]));
+  }
+  
+  return nodes;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,37 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  // primitaves: string
+  if (typeof obj === "string") {
+    return '"' + obj + '"';
+  }
+  //primatives: num & null
+  else if (typeof obj === "number" || obj === null || typeof obj === "boolean") {
+  	return "" + obj;
+  }
+
+  //array
+  else if (Array.isArray(obj)) {
+  	var arr = [];
+  	if (obj.length === 0) {
+  	  return "[]";
+  	} else {
+      obj.forEach(function(item) {
+      	arr.push(stringifyJSON(item));
+      });
+  	}
+  	return "[" + arr + "]";
+  }
+
+  //object
+  else if (typeof obj === "object") {
+    var keyValPairs = [];
+    for (var key in obj) {
+      if (typeof obj[key] === "undefined" || typeof obj[key] === "function") {
+      	continue;
+      }
+      keyValPairs.push(stringifyJSON(key) + ":" + stringifyJSON(obj[key]));
+    }
+    return "{" + keyValPairs + "}";
+  }
 };
